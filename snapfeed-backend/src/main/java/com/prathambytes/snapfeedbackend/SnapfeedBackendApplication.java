08dd2d529c4955034bb3e07ca302faa032ec7da3
prathamkumar <prathamkumar1985@gmail.com>
2023-12-08 23:33:34 +0530

    Latest Commits

---
+++ snapfeed-backend/src/main/java/com/prathambytes/snapfeedbackend/SnapfeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prathambytes.snapfeedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapfeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapfeedBackendApplication.class, args);
+	}
+
+}
---
+++ snapfeed-backend/src/test/java/com/prathambytes/snapfeedbackend/SnapfeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathambytes.snapfeedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapfeedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
